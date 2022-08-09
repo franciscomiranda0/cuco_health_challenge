c4dea07bb48cfa3b25fb1a1dda6d6efb60ffef2b
franciscomiranda0 <franciscomiranda.inbox@gmail.com>
2022-08-08 22:34:52 -0300

    created a http client

---
+++ lib/app/commons/http_client.dart
@@ -0,0 +1,47 @@
+import 'dart:convert' show jsonDecode;
+
+import 'package:http/http.dart' as http show get;
+
+late final HttpClient? _httpClient;
+
+abstract class HttpClientInterface {
+  Future<Map<String, dynamic>> get(
+    String endpoint, {
+    Map<String, dynamic>? queryParameters,
+  });
+}
+
+class HttpClient implements HttpClientInterface {
+  const HttpClient._();
+
+  factory HttpClient() => _httpClient ??= const HttpClient._();
+
+  /// Do not start [endpoint]s with slash as its already provided.
+  @override
+  Future<Map<String, dynamic>> get(
+    String endpoint, {
+    Map<String, dynamic>? queryParameters,
+  }) async {
+    final Map<String, dynamic> response;
+    //Todo: set hardcoded host to env baseUrl.
+    final rawResponse = await http.get(
+      Uri(
+        scheme: 'https',
+        host: 'randomuser.me',
+        path: endpoint,
+        queryParameters: queryParameters,
+      ),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    );
+
+    if (rawResponse.statusCode == 200) {
+      response = jsonDecode(rawResponse.body);
+    } else {
+      throw Exception('The request could not be completed.');
+    }
+
+    return response;
+  }
+}
