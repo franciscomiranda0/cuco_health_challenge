acfe2aff64dc4fe4f72e78a50cf5ed6756d32305
franciscomiranda0 <franciscomiranda.inbox@gmail.com>
2022-08-10 01:30:47 -0300

    added spacing widgets

---
+++ lib/app/commons/widgets/horizontal_spacer.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class HorizontalSpacer extends SizedBox {
+  const HorizontalSpacer({
+    required super.width,
+    super.key,
+  });
+}
---
+++ lib/app/commons/widgets/vertical_spacer.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class VerticalSpacer extends SizedBox {
+  const VerticalSpacer({
+    required super.height,
+    super.key,
+  });
+}
